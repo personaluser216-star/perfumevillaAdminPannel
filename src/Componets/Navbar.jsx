6d0e9f9a7ac9ebc2ec6b1834bd136c8b38ab3418
personaluser216-star <personaluser216@gmail.com>
2025-10-04 15:30:31 +0530

    Update Navbar.jsx

--- src/Componets/Navbar.jsx
+++ src/Componets/Navbar.jsx
@@ -3,12 +3,15 @@ import {assets} from "../assets/assets"
 
 const Navbar = ({setToken}) => {
   return (
-    <div className='flex items-center justify-between py-2 px-[4%] bg-[#f8f7f4]'>
-        <img src={assets.logo} alt=""  className='md:h-20 h-16 '/>
-        <button 
+   <div className='fixed top-0 left-0 w-full flex items-center justify-between py-2 px-[4%] bg-[#f8f7f4] z-50 shadow-md'>
+    <img src={assets.logo} alt="" className='md:h-20 h-16' />
+    <button 
         onClick={()=>setToken('')}
-        className='bg-[#5b4f47] text-white px-4 py-1 sm:px-7 py-2 sm:px-7 sm:py-2 rounded-full'>Logout</button>
-    </div>
+        className='bg-[#5b4f47] text-white px-4 py-1 sm:px-7 sm:py-2 md:mr-0 mr-12 hover:bg-[#473c35] transition-colors duration-200'>
+        Logout
+    </button>
+</div>
+
   )
 }
 
